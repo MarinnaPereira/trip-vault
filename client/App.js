a1766a2a4e837816c9daf524f5b89f4a5e30ed54
MarinnaPereira <marinna.dos.anjos@gmail.com>
2024-04-18 10:50:13 +0200

    added server directory

--- client/App.js
+++ client/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextBase, View } from "react-native";
 
 export default function App() {
   return (
